f5e065aa2a39f707ceb6f47255398347520968a7
Max <71935156+maxoux53@users.noreply.github.com>
2026-08-09 23:43:23 +0200

    chore: clean dev seed user inserts

--- database/scripts/seed-dev.sql
+++ database/scripts/seed-dev.sql
@@ -19,10 +19,10 @@ BEGIN TRANSACTION;
   ('Marchandises', 'C', '0060373a-9189-4ae9-ca33-69def9212a00');
 
   INSERT INTO "user" (first_name, last_name, email, password_hash, is_admin, avatar) VALUES
-  ('Jean', 'Dupont', 'jean.dupont@example.com', '$argon2id$v=19$m=65536,p=4,t=3$F51rid3PPppsIk1Yy4Esmw$UCDS9ilVNYltewLmrZ2SxDDE9GR4QlpwPuXw+ADwP/M', TRUE, '89f983e1-5675-4fa6-510f-afd46e18a200'),  -- password: jeandupont123
-  ('Marie', 'Martin', 'marie.martin@example.com', '$argon2id$v=19$m=65536,p=4,t=3$AairEWOQK9sjnxHOmg5m7w$QD5SJGMJyhQMaaKnglcSPsThCnz4kKFA1zJbzZthrNI', FALSE, '10d761b2-79c9-47a6-f59b-f23d2038df00'),  -- password: mariemartin123
-  ('Pierre', 'Dubois', 'pierre.dubois@example.com', '$argon2id$v=19$m=65536,p=4,t=3$j0F5cssaD0Bwe8ossJA+zA$Uew9KeyUzymuGP4WnP0D0ASukwaNSjiMYe7464/SSpA', FALSE, '7416b5fc-22b6-421f-0018-9d8a66efc700'),  -- password: pierredubois123
-  ('Test', 'User', 'test@dev.com', '$argon2id$v=19$m=65536,p=4,t=3$3M/9Q2ESo5EJGuHTe0Y0lQ$0kpTl5Hor0iUSWQgiNICW3XDvlekBz5jj/lh7/Nkynk', TRUE, '6adabbd1-68be-4301-766d-6663ca48d700');  -- password: test123
+  ('Jean', 'Dupont', 'jean.dupont@example.com', '$argon2id$v=19$m=65536,p=4,t=3$F51rid3PPppsIk1Yy4Esmw$UCDS9ilVNYltewLmrZ2SxDDE9GR4QlpwPuXw+ADwP/M', TRUE, '89f983e1-5675-4fa6-510f-afd46e18a200'),
+  ('Marie', 'Martin', 'marie.martin@example.com', '$argon2id$v=19$m=65536,p=4,t=3$AairEWOQK9sjnxHOmg5m7w$QD5SJGMJyhQMaaKnglcSPsThCnz4kKFA1zJbzZthrNI', FALSE, '10d761b2-79c9-47a6-f59b-f23d2038df00'),
+  ('Pierre', 'Dubois', 'pierre.dubois@example.com', '$argon2id$v=19$m=65536,p=4,t=3$j0F5cssaD0Bwe8ossJA+zA$Uew9KeyUzymuGP4WnP0D0ASukwaNSjiMYe7464/SSpA', FALSE, '7416b5fc-22b6-421f-0018-9d8a66efc700'),
+  ('Test', 'User', 'test@dev.com', '$argon2id$v=19$m=65536,p=4,t=3$3M/9Q2ESo5EJGuHTe0Y0lQ$0kpTl5Hor0iUSWQgiNICW3XDvlekBz5jj/lh7/Nkynk', TRUE, '6e5deb47-3eb8-42d5-87e0-f067b09b0400');
 
   INSERT INTO event (name, location, image, iban) VALUES
   ('Festival d''Été', 'Parc Central', 'ca8d31ae-35c8-4faa-2daa-18c5b33c6d00', 'GB29 NWBK 6016 1331 9268 19'),
