63273e0202508ef81f22ed186c37bfb009bbb7cf
Max <71935156+maxoux53@users.noreply.github.com>
2026-08-10 16:17:24 +0200

    fix: make init script idempotent

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-  CREATE TABLE "user" (
+  CREATE TABLE IF NOT EXISTS "user" (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
@@ -10,7 +10,7 @@ BEGIN TRANSACTION;
     deletion_date TIMESTAMP DEFAULT NULL
   );
 
-  CREATE TABLE event (
+  CREATE TABLE IF NOT EXISTS event (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     location VARCHAR(80) NOT NULL,
@@ -19,13 +19,16 @@ BEGIN TRANSACTION;
     iban VARCHAR(35) NOT NULL
   );
 
-  CREATE TYPE membership_role AS ENUM (
-    'host',
-    'cashier',
-    'guest'
-  );
+  DO $$ BEGIN
+      CREATE TYPE membership_role AS ENUM (
+        'host',
+        'cashier',
+        'guest'
+      );
+  EXCEPTION WHEN duplicate_object THEN NULL;
+  END $$;
 
-  CREATE TABLE membership (
+  CREATE TABLE IF NOT EXISTS membership (
     user_id INT REFERENCES "user"(id),
     event_id INT REFERENCES event(id) ON DELETE CASCADE,
     role membership_role NOT NULL DEFAULT 'guest',
@@ -33,7 +36,7 @@ BEGIN TRANSACTION;
     CONSTRAINT pk_membership PRIMARY KEY (user_id, event_id)
   );
 
-  CREATE TABLE vat (
+  CREATE TABLE IF NOT EXISTS vat (
     "type" CHAR(1) PRIMARY KEY,
     rate SMALLINT NOT NULL,
     deletion_date TIMESTAMP DEFAULT NULL,
@@ -41,7 +44,7 @@ BEGIN TRANSACTION;
     CONSTRAINT chk_rate CHECK (rate >= 0 AND rate <= 100)
   );
 
-  CREATE TABLE category (
+  CREATE TABLE IF NOT EXISTS category (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     label VARCHAR(50) NOT NULL,
     vat_type CHAR(1) NOT NULL REFERENCES vat("type"),
@@ -49,7 +52,7 @@ BEGIN TRANSACTION;
     deletion_date TIMESTAMP DEFAULT NULL
   );
 
-  CREATE TABLE product (
+  CREATE TABLE IF NOT EXISTS product (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     label VARCHAR(80) NOT NULL,
     is_available BOOLEAN NOT NULL DEFAULT TRUE,
@@ -62,14 +65,14 @@ BEGIN TRANSACTION;
     CONSTRAINT chk_price CHECK (excl_vat_price >= 0::MONEY)
   );
 
-  CREATE TABLE purchase (
+  CREATE TABLE IF NOT EXISTS purchase (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL REFERENCES "user"(id),
     is_served BOOLEAN NOT NULL DEFAULT FALSE
   );
 
-  CREATE TABLE order_line (
+  CREATE TABLE IF NOT EXISTS order_line (
     product_id INT REFERENCES product(id),
     purchase_id INT REFERENCES purchase(id) ON DELETE CASCADE,
     quantity SMALLINT NOT NULL,
